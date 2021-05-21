8a89c5446dfb3c1ee2170610989b33f07ff58646
Justin <jdanielmcarthur@msn.com>
2021-05-21 13:54:42 -0700

    commit tree 3791cf62f1186a285ccfc6a6a86a4eec067d27dd

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(x => x.split(" ")).map(y => y.map(word => word[0].toUpperCase() + word.substring(1)).join(" ")) 
 }
+
+// tutorials.map iterates through each title in the array
+// (x => x.split(" ")) transforms each title into its own array 
+// .map iterates through each array 
+// (y => y.map iterates through each word in each array
+// (word => word[0].toUpperCase() each word is transformed into its capital first letter
+// + word.substring(1) concatenates that capital first letter with the rest of the word
